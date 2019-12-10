4a0aa5c5618ea6578d95381de8421623e7445bd8
Yusuf Rizal <yusufrizalh@gmail.com>
2019-12-10 11:10:18 +0700

    react-native-base-io

---
+++ App.js
@@ -0,0 +1,119 @@
+/* eslint-disable jsx-quotes */
+/* eslint-disable quotes */
+/* eslint-disable comma-dangle */
+/* eslint-disable no-undef */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable eol-last */
+/* eslint-disable keyword-spacing */
+/* eslint-disable no-unused-vars */
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable prettier/prettier */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  StatusBar,
+  Image, Alert
+} from 'react-native';
+
+import {
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+// import component from native-base 
+import {Container, 
+  Header, 
+  Button, 
+  Content, 
+  Text, Root, Icon, 
+  ActionSheet, Thumbnail, 
+  Card, CardItem, Body} from "native-base";
+
+import InputFields from './InputFields';
+
+  // opsi-opsi dalam button action sheet 
+  var OPSI = ["Opsi 1", "Opsi 2", "Opsi 3", "Hapus", "Cancel"];
+  var HAPUS_INDEX = 3;
+  var CANCEL_INDEX = 4;
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    // nilai awal belum ada opsi yg dipilih 
+    this.state = {}; 
+  }
+
+  render() {
+    return(
+      // <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      //   <Text style={{fontSize: 28}}>
+      //     Learn Native Base
+      //   </Text>
+      // </View>
+      <Root>
+      <Container>
+        <Header />
+        <Content padder>
+          <Button bordered iconRight primary 
+            onPress={ () => 
+            ActionSheet.show({
+              options: OPSI, 
+              cancelButtonIndex: CANCEL_INDEX, 
+              destructiveButtonIndex: HAPUS_INDEX, 
+              title: "PILIH OPSI"
+            },
+              buttonIndex => {
+                this.setState({clicked: OPSI[buttonIndex]});
+              })}>
+            
+            <Text>Action Sheet</Text>
+
+            <Icon name='home' />
+          </Button>
+
+          {/* Card */}
+          {/* <Card>
+            <CardItem header>
+              <Text>Card Header</Text>
+            </CardItem>
+
+            <CardItem button onPress={() => Alert.alert("Menekan Card Item")}>
+              <Body>
+                <Image source={require('./village.png')}
+                  style={{flex: 1, width: 360, height: 160}} />
+                <Text>
+                  Lorem ipsum dolor sit amet, 
+                  consectetur adipiscing elit. 
+                  Proin elementum finibus neque ut viverra. 
+                  Curabitur sed efficitur est. 
+                  Integer tincidunt quam nec nisi varius, sit.
+                </Text>                
+              </Body>
+              
+            </CardItem>
+
+            <CardItem footer>
+              <Text>Card Footer</Text>
+            </CardItem>
+          </Card> */}
+          <InputFields />
+        </Content>
+        
+      </Container>
+      </Root>
+    );
+  }
+}
---
+++ InputFields.js
@@ -0,0 +1,44 @@
+/* eslint-disable keyword-spacing */
+/* eslint-disable semi */
+/* eslint-disable eol-last */
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable quotes */
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable no-unused-vars */
+import React, {Component} from 'react';
+import {Text, View, TextInput} from 'react-native';
+
+const InputFields = ({
+    name, 
+    customStyle, 
+    onChangeText, 
+    value, 
+    disabled,
+    placeholder, 
+    errors,
+}) => {
+    return(
+        <View>
+            <TextInput 
+                value={value && value} 
+                onChangeText={onChangeText ? (val) => 
+                    onChangeText(val) : null} 
+                placeholder={placeholder ? placeholder : ""}
+                disabled={disabled}
+                style={customStyle ? customStyle : {}} />
+
+            {
+                errors && errors.length > 0 && 
+                    errors.map((item, index) => 
+                        item.field === name && item.error ? 
+                        <Text key={`${index}`} 
+                            style={{color: 'red', marginLeft: 25, 
+                            marginTop: 5}}>
+                            {item.error}
+                        </Text> : null)
+            }
+        </View>
+    );
+}
+export default InputFields;
